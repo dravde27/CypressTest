0069309e2b5b1f06f5b9601d2795772b638a919b
edwardkvasenko <dravde27@gmail.com>
2023-04-27 20:49:33 +0300

    kek111

--- cypress/e2e/eduardotests/todo7.cy.js
+++ cypress/e2e/eduardotests/todo7.cy.js
@@ -5,7 +5,7 @@ Cypress.on('uncaught:exception',(err, runnable)=>{
 describe('example to-do app', () => {
 
   it('testik', () => {
-      
+       let authorization1 = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwMDM1NTAwMDAxTVhLdGhBQUgiLCJjb21wYW55SWQiOiIwMDE1NTAwMDAxWFM3dXpBQUQiLCJjb21wYW55TmFtZSI6IklkZWFsIFJvb2ZpbmcgTExDIiwicm9sZSI6IkFkbWluIiwiYXV0aG9yaXplZFRvU2VsbCI6IlRydWUiLCJlbWFpbCI6Imxla2ljZXI1OTdAb2N0b3ZpZS5jb20iLCJhc3Npc3RlZEJ5QWRtaW4iOiJGYWxzZSIsImV4cCI6MTY4MjUwODg1MywiaXNzIjoiZ2FmZS1wb3J0YWwtY2VydC0xIiwiYXVkIjoiZ2FmZS1wb3J0YWwtY2VydC0xIn0.oKO2fHeNnA6twv5193FQr_rPKp123ElNPlw9GXGm9ek'
      cy.readFile('cypress/support/porsche.jpg')
      .then(file => {
       cy.request({
@@ -15,7 +15,7 @@ describe('example to-do app', () => {
          'content-type': 'multipart/form-data',
           authorization:authorization1,
         },
-       // failOnStatusCode: false,
+        failOnStatusCode: false,
         form: true,
         body: {
           BucketId: 'a0T5500000LE3U7EAL',
